37cf7b9e55f1868f6aacf9120fbe7b554f2d11ac
Keksonoid <keksonoid98@gmail.com>
2022-05-23 16:01:56 +0600

    fix: change get request to post

--- backend/mlapi.py
+++ backend/mlapi.py
@@ -3,14 +3,17 @@ import uvicorn
 import pickle
 from feature_extraction import generate_data_set
 import numpy as np
+from pydantic import BaseModel
 
+class Item(BaseModel):
+    url: str
 
 app = FastAPI(debug=True)
 
-@app.get('/check')
-def predict(url:str):
+@app.post('/check')
+def predict(item: Item):
     model = pickle.load(open("model.dat", "rb"))
-    data = generate_data_set(url)
+    data = generate_data_set(item.url)
     formated_data = np.reshape(data, (1,30))
     predicted_data = model.predict(formated_data)
 
